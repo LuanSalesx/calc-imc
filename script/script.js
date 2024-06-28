eac443ebf85c0b0776a42fd9ebf13cc23f8c7e2b
Luan <165568493+LuanSalesx@users.noreply.github.com>
2024-06-28 09:40:21 -0300

    adicionado as pasta do projeto.

---
+++ script/script.js
@@ -0,0 +1,37 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc(){
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== ''){
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if ( valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.';
+        } else if (valorIMC < 25){
+            classificacao = 'com o Peso normal. Parabéns🎉';
+        } else if (valorIMC < 30){
+            classificacao = 'Acima do peso!';
+        } else if (valorIMC < 35){
+            classificacao = 'com obesidade grau I. !!';
+        } else if (valorIMC < 40){
+            classificacao = 'com obesidade grau II. !!';
+        } else {
+            classificacao = 'com obesidade grau III. Cuidado!';
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    } else {
+        resultado.textContent = 'Preencha todos os campos!';
+    }
+}
+
+calcular.addEventListener('click', imc);
